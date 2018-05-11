4ddfbf2c168138b703805f03c51353e80081d736
Wah <133313829@qq.com>
2018-05-12 00:52:45 +0800

    添加了单元测试

--- src/main/java/com/crm/core/sensitive/service/SensitiveWordService.java
+++ src/main/java/com/crm/core/sensitive/service/SensitiveWordService.java
@@ -5,6 +5,8 @@ import org.wah.doraemon.entity.consts.UsingState;
 import org.wah.doraemon.security.request.Page;
 import org.wah.doraemon.security.request.PageRequest;
 
+import java.util.List;
+
 public interface SensitiveWordService{
 
     void save(SensitiveWord sensitiveWord);
@@ -16,4 +18,8 @@ public interface SensitiveWordService{
     Page<SensitiveWord> page(PageRequest pageRequest, String content, UsingState state);
 
     String getRegEx(String wxno);
+
+    void updateRelationByGroupId(String groupId, List<String> sensitiveIds);
+
+    void updateRelationByWechatId(String wechatId, List<String> sensitiveIds);
 }
--- src/main/java/com/crm/core/sensitive/service/SensitiveWordServiceImpl.java
+++ src/main/java/com/crm/core/sensitive/service/SensitiveWordServiceImpl.java
@@ -11,7 +11,10 @@ import org.wah.doraemon.entity.consts.UsingState;
 import org.wah.doraemon.security.request.Page;
 import org.wah.doraemon.security.request.PageRequest;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
@@ -57,15 +60,35 @@ public class SensitiveWordServiceImpl implements SensitiveWordService{
 
         //根据微信号查询设备所有敏感词
         List<SensitiveWord> sensitives = sensitiveWordDao.findByWxno(wxno, UsingState.USABLE);
+        //去重
+        Set<SensitiveWord> set = new HashSet<SensitiveWord>(sensitives);
 
         StringBuffer sb = new StringBuffer();
-        for(int i = 0; i < sensitives.size(); i++){
-            if(i > 0){
+        Iterator<SensitiveWord> iterator = set.iterator();
+        while(iterator.hasNext()){
+            sb.append(CharUtils.toUnicode(iterator.next().getContent()));
+
+            if(iterator.hasNext()){
                 sb.append('|');
             }
-            sb.append(CharUtils.toUnicode(sensitives.get(i).getContent()));
         }
 
         return sb.toString();
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void updateRelationByGroupId(String groupId, List<String> sensitiveIds){
+        Assert.hasText(groupId, "分组ID不能为空");
+
+        sensitiveWordDao.updateRelationByGroupId(groupId, sensitiveIds);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void updateRelationByWechatId(String wechatId, List<String> sensitiveIds){
+        Assert.hasText(wechatId, "微信ID不能为空");
+
+        sensitiveWordDao.updateRelationByWechatId(wechatId, sensitiveIds);
     }
 }
