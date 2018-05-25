76c0f330fe397345fef2e1656a65a61035ff24b8
LyhHoiRab <133313829@qq.com>
2018-05-25 18:15:44 +0800

    通过拦截器配置跨域请求

---
+++ src/main/java/com/crm/commons/security/interceptor/CorsInterceptor.java
@@ -0,0 +1,21 @@
+package com.crm.commons.security.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CorsInterceptor extends HandlerInterceptorAdapter{
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        //所有域名
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        //所有请求头
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        //所有请求类型
+        response.setHeader("Access-Control-Allow-Methods", "*");
+
+        return true;
+    }
+}
